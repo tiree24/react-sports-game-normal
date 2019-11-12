fcde4d13095cb7c190aca7add6e3e3868f3d205f
petem14 <petem14@gmail.com>
2019-11-12 15:54:20 -0500

    initial commit

--- reactApp.js
+++ reactApp.js
@@ -1,16 +1,57 @@
+class Team extends React.Component {
+  constructor(props) {
+    super(props);
 
-// Deafault App component that all other compents are rendered through
-function App(props){
-  return (
-    <div>
-      <h1>Welcome to the sports game starter</h1>
-      This file represents the code after completing the setup step in the lab instructions
-    </div>
-  )
+    this.state = {
+      shots: 0,
+      score: 0
+    };
+  }
+
+  shotHandler = () => {
+    let score = this.state.score;
+
+    if (Math.random() > 0.5) {
+      score += 1;
+    }
+
+    this.setState((state, props) => ({
+      shots: state.shots + 1,
+      score
+    }));
+  };
+
+  render() {
+    return (
+      <div className="Team">
+        <h2>{this.props.name}</h2>
+        <div className="identity">
+          <img src={this.props.logo} alt={this.props.name} />
+        </div>
+        <div>
+          <strong>Shots:</strong> {this.state.shots}
+        </div>
+        <div>
+          <strong>Score:</strong> {this.state.score}
+        </div>
+        <button onClick={this.shotHandler}>Shoot!</button>{" "}
+      </div>
+    );
+  }
 }
 
-//Render the application
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+// Deafault App component that all other compents are rendered through
+function App(props) {
+  return (
+    <div className="App">
+      <Team name="Russiaville Raccoons" logo="./assets/img/raccoon.jpeg" />
+      <div className="versus">
+        <h1>VS</h1>
+      </div>
+
+      <Team name="Sheridan Squirrels" logo="./assets/img/squirrel.jpeg" />
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
