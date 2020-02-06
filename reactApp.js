54eeb24c5108dd10f232b36ea9ab5f1d57af295d
Jackson <tireej@amazon.com>
2020-02-06 11:26:55 -0500

    update

--- reactApp.js
+++ reactApp.js
@@ -35,6 +35,23 @@ function Team(props) {
 
 }
 
+function ScoreBoard(props) {
+  return (
+    <div className="ScoreBoard">
+      <div className="teamStats">
+        <h3>VISITORS</h3>
+        <h3>{props.visitingTeamStats.score}</h3>
+      </div>
+
+      <h3>SCOREBOARD</h3>
+
+      <div className="teamStats">
+        <h3>HOME</h3>
+        <h3>{props.homeTeamStats.score}</h3>
+      </div>
+    </div>
+  )
+}
 
 class Game extends React.Component {
   constructor(props) {
@@ -99,6 +116,10 @@ class Game extends React.Component {
 
     return (
       <div className="Game">
+        <ScoreBoard
+        visitingTeamStats={this.state.visitingTeamStats}
+        homeTeamStats={this.state.homeTeamStats}
+      />
         <h1>Welcome to {this.props.venue}</h1>
         <div className="stats">
           <Team
